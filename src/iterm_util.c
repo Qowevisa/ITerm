c374d6be7d68aeb824e632aea2470066428f4772
Qowevisa <qowevisa@gmail.com>
2022-10-14 22:07:43 +0300

    Add validation for tcgetattr and tcsetattr

--- src/iterm_util.c
+++ src/iterm_util.c
@@ -1,6 +1,8 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
+#include "../inc/iterm_error.h"
 
 struct termios mem;
 
@@ -8,9 +10,13 @@ void
 toggle_echo()
 {
     struct termios out_t;
-    // FIXME add validations on func output
-    tcgetattr(0, &out_t);
+    int32_t ret;
+    if ((ret = tcgetattr(0, &out_t)) != 0) {
+        raise_error_func(tcgetattr(0, &out_t), ret, 0);
+    }
     out_t.c_lflag ^= ECHO;
     out_t.c_lflag ^= ICANON;
-    tcsetattr(0, TCSANOW, &out_t);
+    if ((ret = tcsetattr(0, TCSANOW, &out_t)) != 0) {
+        raise_error_func(tcsetattr(0, TCSANOW, &out_t), ret, 0);
+    }
 }
